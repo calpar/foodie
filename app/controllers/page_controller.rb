7346bbf74cee7b22f36e4d25f9802888e880d13f
calpar <calpar@gmail.com>
2012-05-11 20:44:56 +0100

    Page controlleri generate edildi

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,10 @@
+class PageController < ApplicationController
+  def home
+  end
+
+  def product
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodie::Application.config.secret_token = 'eaca1cabad9021c17c5e7aad3902fe7fcda940818065eb7f0d489cd583e5cbdb2b06a932e7a69161eb487c7e88bfd52878df9697ed65d67038909a2112e6cbe7'
